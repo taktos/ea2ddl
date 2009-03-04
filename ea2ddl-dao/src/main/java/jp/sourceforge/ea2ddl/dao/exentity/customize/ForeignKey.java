308dc3135d25564d22e79e19f9003432ecca3e84
taktos9 <taktos9@136db618-7844-41ca-8ac1-fb3fd040db1d>
2009-03-04 01:51:07 +0000

    DBFlute-0.9.3に更新
    git-svn-id: http://svn.sourceforge.jp/svnroot/ea2ddl/trunk/ea2ddl@78 136db618-7844-41ca-8ac1-fb3fd040db1d

---
+++ ea2ddl-dao/src/main/java/jp/sourceforge/ea2ddl/dao/exentity/customize/ForeignKey.java
@@ -0,0 +1,15 @@
+package jp.sourceforge.ea2ddl.dao.exentity.customize;
+
+/**
+ * The entity of ForeignKey.
+ * <p>
+ * You can implement your original methods here.
+ * This class remains when re-generating.
+ * </p>
+ * @author DBFlute(AutoGenerator)
+ */
+public class ForeignKey extends jp.sourceforge.ea2ddl.dao.bsentity.customize.BsForeignKey {
+
+    /** Serial version UID. (Default) */
+    private static final long serialVersionUID = 1L;
+}
